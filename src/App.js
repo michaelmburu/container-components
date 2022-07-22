7028d67db94eb5619422069ae74913e08fd35f66
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-22 14:58:44 +0300

    - Generic DataSource component that loads data from any source and displays in a child component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import axios from 'axios';
 import './App.css';
+import { DataSource } from './DataSource';
 import { ProductInfo } from './ProductInfo';
 import { ResourceLoader } from './ResourceLoader';
 import { UserInfo } from './UserInfo';
 
-import { UserLoader } from './UserLoader';
+
+const getServerData = url => async () => {
+  const response = await axios.get(url);
+  return response.data
+}
+
+const getLocalStorageData = key => () => {
+  return localStorage.getItem(key);
+}
+
+const Text = ({message}) => <h1>{message}</h1>
 
 // Container components using a generic resource loader component that loads to child components
 function App() {
   return (
     <>
-      <ResourceLoader resourceUrl="/users/123" resourceName="user">
+      <DataSource getDataFunction={getLocalStorageData('message')} resourceName='message'>
+        <Text />
+      </DataSource>
+      <DataSource getDataFunction={getServerData('/users/123')} resourceName="user" >
         <UserInfo />
-      </ResourceLoader>
+      </DataSource>
       <ResourceLoader resourceUrl="/products/1234" resourceName="product">
         <ProductInfo />
       </ResourceLoader>
